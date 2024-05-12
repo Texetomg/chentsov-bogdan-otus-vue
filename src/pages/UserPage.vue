6ebe69a7072649557b6addbfa9788a67df60593e
texetomg <texetomg@gmail.com>
2024-05-12 14:54:39 +0300

    Work on user soluitions.

--- src/pages/UserPage.vue
+++ src/pages/UserPage.vue
@@ -1,14 +1,32 @@
-<script setup lang="ts">
-import { useAuthStore } from 'src/stores/auth';
-const store = useAuthStore();
-const { authInfo } = store;
-</script>
-
 <template>
   <q-page class="row items-center">
-    <div>
-      <div>login: {{ authInfo.login }}</div>
-      <div>email: {{ authInfo.email }}</div>
-    </div>
+    <q-card class="q-pa-md">
+      <div class="q-pb-md">Login: {{ authInfo.login }}</div>
+      <div class="q-pb-md">Email: {{ authInfo.email }}</div>
+      <div v-if="solutions" class="q-pb-sm">Solutions:</div>
+      <li v-for="solution in solutions" :key="solution.name">
+        <a :href="`tasks/${solution.task.id}`"> {{ solution.task.name }} </a>
+      </li>
+    </q-card>
   </q-page>
 </template>
+
+<script setup lang="ts">
+import { api } from 'src/boot/axios';
+import { useAuthStore } from 'src/stores/auth';
+import { onMounted, ref } from 'vue';
+
+const store = useAuthStore();
+const { authInfo } = store;
+const solutions = ref(null);
+
+const fetchSolutions = () => {
+  api.get('solutions/user').then((response) => {
+    solutions.value = response?.data?.solution;
+  });
+};
+
+onMounted(() => {
+  fetchSolutions();
+});
+</script>
